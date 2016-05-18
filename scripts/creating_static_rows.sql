5250383244f868c5ac783aaad3fbf5ad3516b18b
Arturs Anohins <arturs.anohins@gmail.com>
2016-05-18 16:35:40 +0300

    added new scripts, removed diagrams folder

---
+++ scripts/creating_static_rows.sql
@@ -0,0 +1,18 @@
+-- INSERT INTO user_type (Type_name) VALUES ('student'),('professor');
+	
+
+INSERT INTO user(username,password,active,name,surname)
+VALUES ('mkyong','123456', true,'a','a');
+INSERT INTO user(username,password,active,name,surname)
+VALUES ('alex','123456', true,'a','a');
+select * from user;
+delete from user where ID>2;
+insert into user_type(user_id,Type_name)
+Values ((SELECT ID from user WHERE username='mkyong'),'professor');
+insert into user_type(user_id,Type_name)
+Values ((SELECT id from user WHERE username='alex'),'student');
+select * from user_type;
+
+
+INSERT INTO grade(Title,Grade,Date,Description,Student_ID,Task_ID,Group_ID)
+VALUES ('Title',10, 'Date','Description',2,1,1);
